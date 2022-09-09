b3ad25baef6a25575753addf4191486c1f21ddc8
3nsar <84131267+3nsar@users.noreply.github.com>
2022-09-09 12:24:52 +0200

    some files

---
+++ app.js
@@ -0,0 +1,18 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let result = document.getElementById("result");
+let number = 0;
+
+btn1.addEventListener("click",function(){
+    let newNumber = --number;
+    result.innerText = newNumber;
+})
+
+btn2.addEventListener("click",function(){
+    let newNumber = ++number;
+    result.innerText = newNumber;
+})
+btn3.addEventListener("click",function(){
+    result.innerText = 0;
+})
